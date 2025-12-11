010a374987597af2823df42aebb4ca52935bfa00
Leonardo <lptleo11@gmail.com>
2025-12-10 20:22:36 -0400

    feat: add app-login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('rust');
+
+  constructor(private router: Router) {}
+
+  isLoginRoute() {
+    if (this.router.url === "login") {
+      return true
+    } else {
+      return false
+    }
+  }
 }
