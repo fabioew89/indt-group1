fb5e3adc81fe9095c0997475556c836bc1212438
Leonardo <lptleo11@gmail.com>
2025-12-22 15:13:16 -0400

    feat(dashboard): enhance dashboard with tracked items display and search functionality

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from '../../core/components/header/header';
 import { RouterModule } from '@angular/router';
+import { TrackedItems } from '../../shared/services/tracked-items';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,5 +10,5 @@ import { RouterModule } from '@angular/router';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-
+  trackedItems = inject(TrackedItems);
 }
